59d982d3eb762e1aec3f255a04b1b2bb7d3a6bd3
Mohamed Santawi <mohamedsant3@gmail.com>
2025-04-30 18:00:37 +0300

    3042025

--- src/productsShow.js
+++ src/productsShow.js
@@ -13,7 +13,7 @@ import ceaser from "./assets/ceaser.png";
 import camera from "./assets/camera.png";
 import cream from "./assets/cream.png";
 import foot from "./assets/foot.png";
-import car from "./assets/car.png";
+import carImage from "./assets/car.png";
 
 const toArabicNumbers = (str) => {
   const arabicNumbers = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
@@ -232,7 +232,7 @@ const products = [
     discountAr: toArabicNumbers("0"),
     stars: 5,
     rating: 96,
-    image: car,
+    image: carImage,
     newProduct: "new",
     colors: "#FB1314",
   },
